769c0d20df01546b3465b5820c8d68ac378a7425
facobackup <gustavomicaelbarbosa@gmail.com>
2022-10-08 19:03:50 -0300

    Native context menu; Fixed About.svelte component

--- frontend/components/context-menu/ContextMenu.svelte
+++ frontend/components/context-menu/ContextMenu.svelte
@@ -1,37 +1,23 @@
 <script>
     import {onDestroy, onMount} from "svelte";
-    import Options from "./components/Options.svelte";
     import ContextMenuController from "../../libs/ContextMenuController";
 
+
+    const {ipcRenderer} = window.require("electron")
     const RIGHT_BUTTON = 2
 
     let startPosition = undefined, locked = false
     let contextMenu
-    let contextProps = {}
+
     let open = false
 
     function checkMouseOffset(startPosition, event) {
         return Math.abs(startPosition.x - event.clientX) < 10 && Math.abs(startPosition.y - event.clientY) < 10
     }
 
-    function setPlacementOffset(target, event) {
-        const bBox = target.getBoundingClientRect()
-        if (event.clientX + bBox.width > document.body.offsetWidth)
-            target.style.left = ((event.clientX - 8) - bBox.width / 2) + "px"
-        else
-            target.style.left = (event.clientX - 8) + "px"
-
-        if (event.clientY + bBox.height > document.body.offsetHeight) {
-            target.style.top = (event.clientY + 8) - bBox.height + "px"
-        } else
-            target.style.top = (event.clientY - 8) + "px"
-        target.style.zIndex = "999"
-
-    }
 
     const handleContext = (event) => {
         if (startPosition && !locked && ContextMenuController.data.focused) {
-            console.log("HERE")
             event.preventDefault()
             if (checkMouseOffset(startPosition, event)) {
                 let targetElement
@@ -72,20 +58,9 @@
                         })
 
                     open = true
-                    contextProps = {
-                        metadata: ContextMenuController.data.focused.metadata,
-                        options: ContextMenuController.data.focused.options,
-                        onFocus: ContextMenuController.data.focused.onFocus,
-                        selected: targetElement,
-                        trigger,
-                        event,
-                        close: () => {
-                            open = false
-                            contextMenu.style.zIndex = "-1"
-                        },
-                        callback: () => setPlacementOffset(contextMenu, event),
-                        open: true
-                    }
+                    if(ContextMenuController.data.focused.onFocus)
+                        ContextMenuController.data.focused.onFocus(trigger, targetElement, event)
+                    ipcRenderer.send("OPEN_CONTEXT_MENU", ContextMenuController.data.focused.id)
                 }
             }
             startPosition = undefined
@@ -97,7 +72,6 @@
             const elements = document.elementsFromPoint(event.clientX, event.clientY)
             let focused
             for (let i = 0; i < elements.length; i++) {
-                console.log(elements[i], ContextMenuController.data.targets)
                 if (!ContextMenuController.data.targets[elements[i].id])
                     continue
                 focused = ContextMenuController.data.targets[elements[i].id]
@@ -126,28 +100,4 @@
 
 </script>
 
-<div
-        class="wrapper"
-        bind:this={contextMenu}
->
-    {#if open}
-        <Options {...contextProps}/>
-    {/if}
-</div>
-
-<style>
-
-    .wrapper {
-        position: absolute;
-        z-index: -1;
-        background: var(--pj-background-tertiary);
-        border: var(--pj-border-primary) 1px solid;
-        width: 225px;
-        max-height: 50vh;
-        border-radius: 3px;
-        overflow-x: hidden;
-        overflow-y: auto;
-        box-shadow: var(--pj-boxshadow);
-    }
-
-</style>
+<div style="display: none" bind:this={contextMenu}></div>
